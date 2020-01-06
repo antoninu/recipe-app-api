a6954f804cded0e8c28f66bdac6dd4f07979a0be
Antonio Aspite <antonioaspite@gmail.com>
2020-01-05 20:45:35 -0500

    fixed linting

--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_succesful(self):
@@ -30,7 +31,10 @@ class ModelTests(TestCase):
 
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
-        user = get_user_model().objects.create_superuser('antonioaspite@gmail.com', "admin123")
+        user = get_user_model().objects.create_superuser(
+            'antonioaspite@gmail.com',
+            "admin123"
+        )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
