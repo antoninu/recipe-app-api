61ae1327dd30ab81339b65c1c4716c2daea3155a
Antonio Aspite <antonioaspite@gmail.com>
2020-01-08 11:24:43 -0500

    updated admin to support custom user model

--- app/core/tests/tests_models.py
+++ app/core/tests/tests_models.py
@@ -20,7 +20,7 @@ class ModelTests(TestCase):
     def tests_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
         email = 'test@GMAIL.COM'
-        user = get_user_model().objects.create_user(email, '123123123')
+        user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
 
